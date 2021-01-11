32853e4bbd9321c305de39d4f67172a90df749f0
dboren <danielboren@att.net>
2021-01-11 17:12:45 -0600

    duplicated writepassword function below my original code

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -99,4 +100,12 @@ function generatePassword() {
       console.log(password);
 
     }
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
