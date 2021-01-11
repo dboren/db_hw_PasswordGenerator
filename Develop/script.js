eb1981044f479bd0f34e42cdab8b6ab1407ff6bc
dboren <danielboren@att.net>
2021-01-10 18:45:51 -0600

    Added character set modular arrays

--- Develop/script.js
+++ Develop/script.js
@@ -21,10 +21,16 @@ function generatePassword() {
     } var hasUppercase = confirm("Do you want uppercase characters in your password?");
       var hasNumbers = confirm("Do you want numbers in your password?");
       var hasSpecChars = confirm("Do you want special characters in your password?");
+
+      var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+      var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+      var numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      var specCharArr = [' ', '!', '#', '$', '%', '(', ')', '*', '+', '-', '.', ':', '<', '>', '?', '@', '^', '_', '|', '~'];
       
     };
 
-console.log(generatePasswod());
-  
+
+
+
 
 
