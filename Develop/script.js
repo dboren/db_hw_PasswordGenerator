6f9ef3a260dbb2880572c9cb441c20475e540a41
dboren <danielboren@att.net>
2021-01-10 22:13:22 -0600

    debugging; determined that only last statement in if else chain is allowing pwdArr to log to console (if proceeding are commented out, the remaining last statement still runs as expected)

--- Develop/script.js
+++ Develop/script.js
@@ -24,6 +24,11 @@ function generatePassword() {
       var hasNumbers = confirm("Do you want numbers in your password?");
       var hasSpecChars = confirm("Do you want special characters in your password?");
 
+        console.log(hasLowercase);
+        console.log(hasUppercase);
+        console.log(hasNumbers);
+        console.log(hasSpecChars);
+
       var lowercaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
       var uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
       var numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -44,7 +49,7 @@ function generatePassword() {
         var pwdArr = specCharArr;
 
     } else if (hasLowercase === true && hasUppercase === true && hasNumbers !== true && hasSpecChars !== true) {
-        var pwdArr = lowercassArr.concat(uppercaseArr);
+        var pwdArr = lowercaseArr.concat(uppercaseArr);
 
     } else if (hasLowercase === true && hasUppercase !== true && hasNumbers === true && hasSpecChars !== true) {
         var pwdArr = lowercaseArr.concat(numbersArr);
@@ -64,7 +69,7 @@ function generatePassword() {
     } else if (hasLowercase === true && hasUppercase === true && hasNumbers === true && hasSpecChars !== true) {
         var pwdArr = lowercaseArr.concat(uppercaseArr, numbersArr);
 
-    }  else if (hasLowercase === true && hasUppercase === true && hasNumbers !== true && hasSpecChars === true) {
+    } else if (hasLowercase === true && hasUppercase === true && hasNumbers !== true && hasSpecChars === true) {
         var pwdArr = lowercaseArr.concat(uppercaseArr, specCharArr);
 
     } else if (hasLowercase !== true && hasUppercase === true && hasNumbers === true && hasSpecChars === true) {
@@ -72,6 +77,9 @@ function generatePassword() {
 
     } else if (hasLowercase === true && hasUppercase === true && hasNumbers === true && hasSpecChars === true) {
         var pwdArr = lowercaseArr.concat(uppercaseArr, numbersArr, specCharArr);
+
+
+
 
         console.log(pwdArr);
 
